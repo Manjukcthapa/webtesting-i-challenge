679d1ddbf094fc8cbd681981e4ab7c0453546782
Manju KC <Manju KC>
2019-06-17 15:37:13 -0400

    repair passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
 const enhancer = require('./enhancer.js');
 
+describe('restores durability to 100', () => {
+ describe('repair()', () =>{
+it('restores durability to 100', () => {
+    expect(enhancer.repair({durability: 50}).durability).toBe(100);
+    expect(enhancer.repair({durability: -10}).durability).toBe(100);
+    expect(enhancer.repair({durability:100}).durability).toBe(100);
+    expect(enhancer.repair({durability:0})).toEqual({durability:100});
+})
+ })   
+})
